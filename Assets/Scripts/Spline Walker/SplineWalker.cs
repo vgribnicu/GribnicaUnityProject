7efe1bc3592d073a0cb34170f510f5c9c615507e
zhenyaprivet <evgenia.bezginova@gmail.com>
2021-08-11 23:32:17 +0300

    vr spline movement

--- Assets/Scripts/Spline Walker/SplineWalker.cs
+++ Assets/Scripts/Spline Walker/SplineWalker.cs
@@ -6,13 +6,10 @@ using UnityEngine.UI;
 
 public class SplineWalker : MonoBehaviour
 {
-	public bool inputEnabled = true;
 	public Vector3 target;
 	public BezierSpline spline;
 
 	public float scrollSensitivity = 0.75f;
-
-	public Slider slider;
 
 	public bool lookForward;
 
@@ -30,9 +27,6 @@ public class SplineWalker : MonoBehaviour
 	public float yMin = 0f;
 	public float yMax = 0f;
 	public float yPrev = 0f;
-	
-	public float startAngle;
-	public float endAngle;
 
 	public float goalProgress = 0f;
 	private Quaternion goalRotation;
@@ -41,81 +35,23 @@ public class SplineWalker : MonoBehaviour
 	
 	private float startPos;
 	private float lerpSpeed;
-	private float start = 0.6f;
-	private float end = 0.8f;
-
-	private bool isMobile;
-	private string OSName = "OS";
-	private string browserName = "Browser";
 	private Vector3 _mousePos;
 
-#if !UNITY_EDITOR && UNITY_WEBGL
-        [System.Runtime.InteropServices.DllImport("__Internal")]
-        static extern bool IsMobile();
-	
-		[System.Runtime.InteropServices.DllImport("__Internal")]
-        static extern string GetOSName();
-    
-        [System.Runtime.InteropServices.DllImport("__Internal")]
-        static extern string GetBrowserName();
-#endif
-
-	void Start()
-	{
-#if !UNITY_EDITOR && UNITY_WEBGL
-            isMobile = IsMobile();
-            OSName = GetOSName();
-            browserName = GetBrowserName();
-
-            if (!isMobile && browserName.Equals("Chrome"))
-            {
-	            if (OSName.Equals("Windows OS"))
-	            {
-		            scrollSensitivity = 2f;
-	            } else if (OSName.Equals("Macintosh"))
-	            {
-		            scrollSensitivity = 0.05f;
-	            }
-            }
-#endif
-	slider.onValueChanged.AddListener(delegate {ScrollSensitivity(slider.value); });
-	ScrollSensitivity(slider.value);
-
-	}
 	void Update()
 	{
-		if (isMobile)
+		
+		float k = 0;
+		if (Input.GetAxis("Mouse ScrollWheel") != 0)
 		{
-			if (Input.touchCount > 0)
-			{
-				Touch touch = Input.GetTouch(0);
-
-				if (touch.phase == TouchPhase.Began)
-				{
-					startPos = touch.position.y;
-				}
-
-				if (touch.phase == TouchPhase.Moved)
-				{
-					zoom += 7 * (touch.position.y - startPos) / Screen.height;
-					startPos = touch.position.y;
-				}
-			}
+			k = Input.GetAxis("Mouse ScrollWheel");
 		}
-		else if (inputEnabled)
+		else if (Input.GetAxis("Vertical") != 0)
 		{
-			float k = 0;
-			if (Input.GetAxis("Mouse ScrollWheel") != 0)
-			{
-				k = Input.GetAxis("Mouse ScrollWheel");
-			}
-			else if (Input.GetAxis("Vertical") != 0)
-			{
-				k = Input.GetAxis("Vertical") * 0.04f;
-			}
+			k = Input.GetAxis("Vertical") * 0.04f;
+		}
 			
-			zoom += k  * scrollSensitivity;
-		}
+		zoom += k  * scrollSensitivity;
+		
 		zoom = Mathf.Clamp(zoom, 0f, 32.5f);
 		zoom01 = zoom / 32.5f;
 		zoom01 = Mathf.Clamp01(zoom01);
@@ -131,7 +67,6 @@ public class SplineWalker : MonoBehaviour
 		
 		Vector3 position = spline.GetPoint(progress);
 		transform.localPosition = position;
-
 		
 
 		if (lookForward)
@@ -154,11 +89,6 @@ public class SplineWalker : MonoBehaviour
 	{
 		 _mousePos = mainCamera.ScreenToViewportPoint(Input.mousePosition);
 		 return  Mathf.Lerp(-1f, 1f,  Mathf.InverseLerp(0, 1, pos));
-	}
-	
-	public void ScrollSensitivity (float value)
-	{
-		scrollSensitivity = value;
 	}
 
 	public void SetLookForward()
---
+++ Assets/Scripts/Spline Walker/SplineWalkerVR.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineWalkerVR : MonoBehaviour
+{
+    public BezierSpline spline;
+    public Vector3 target;
+    public float duration;
+
+    public float progress;
+    public bool lookForward;
+    private float lerpSpeed = 0.7f;
+
+    private void Update()
+    {
+        progress += Time.deltaTime / duration;
+        if (progress > 1f)
+        {
+            progress = 1f;
+        }
+
+        Vector3 position = spline.GetPoint(progress);
+        transform.localPosition = position;
+        Quaternion targetRotation;
+        if (progress > 0.5f)
+        {
+            targetRotation = Quaternion.LookRotation( spline.GetDirection(progress) + position - transform.position);
+        }
+        else
+        {
+          targetRotation = Quaternion.LookRotation( target  - transform.position);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, lerpSpeed);
+    }
+}
